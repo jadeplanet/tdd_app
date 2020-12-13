c0e7cb3324d3e63976c8416d76d80adad339976e
jadeplanet <jade_springreen@naver.com>
2020-12-13 15:13:11 +0900

    CREATE unit test

--- controller/products.js
+++ controller/products.js
@@ -1,3 +1,6 @@
-exports.hello = (req, res) => {
-	res.send('안녕하세요');
+const productModel = require('../models/Product');
+
+exports.createProduct = (req, res, next) => {
+	const createdProduct = productModel.create(req.body);
+	res.status(201).json(createdProduct);
 };
--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const PORT = 5000;
 
 const app = express();
 const productRoutes = require('./routes');
+const mongoose = require('mongoose');
+
+mongoose
+	.connect(
+		'mongodb+srv://jadejang:vYnfCsfPlhxVWr9M@cluster0.69hjx.mongodb.net/<dbname>?retryWrites=true&w=majority',
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	)
+	.then(() => console.log('MongoDB Connected...'))
+	.catch((err) => console.log(err));
 
 app.use(express.json());
 app.use('/api/products', productRoutes);
